dc036a60327e2f626857540c35577ecbe387994b
Gagan740 <gagan@codehelperz.com>
2017-09-18 08:50:19 +0530

    demo

---
+++ js/routes.js
@@ -0,0 +1,24 @@
+/**
+ * Defines the main routes in the application.
+ * The routes you see here will be anchors '#/' unless specifically configured otherwise.
+ */
+
+define(['./app'], function (app) {
+    'use strict';
+    return app.config(['$routeProvider','$locationProvider', function ($routeProvider, $locationProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'partials/partial1.html',
+            controller: 'MyCtrl1'
+        });
+
+        $routeProvider.when('/view2', {
+            templateUrl: 'partials/partial2.html',
+            controller: 'MyCtrl2'
+        });
+
+        $routeProvider.otherwise({
+            redirectTo: '/view1'
+        });
+        $locationProvider.html5Mode(false).hashPrefix('');
+    }]);
+});
